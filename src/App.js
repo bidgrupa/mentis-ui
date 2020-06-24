122c388ea71980669b40ba0825d42f69cd20e0c1
Igor Dudic <igor.dudic@bid.hr>
2020-06-24 19:05:40 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import React from 'react';
+import React, {useState}     from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
+    const [count, setCount] = useState(0);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. Count: {count}
         </p>
-        <a
+        <button
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          onClick={() => {setCount(count + 1)}}
         >
           Learn React
-        </a>
+        </button>
+    <button
+    className="App-link"
+    onClick={() => {setCount(count - 1)}}
+>
+    -1
+    </button>
       </header>
     </div>
   );
